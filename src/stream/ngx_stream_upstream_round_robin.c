1adc3a8b7cb83f093d8f0092e9f95244050713af
Luigi Pressello <luigi@pressello.com>
2017-06-18 10:54:41 +0000

    Patched source code for ngx_stream_upstream_check module

--- src/stream/ngx_stream_upstream_round_robin.c
+++ src/stream/ngx_stream_upstream_round_robin.c
@@ -9,6 +9,9 @@
 #include <ngx_core.h>
 #include <ngx_stream.h>
 
+#if (NGX_STREAM_UPSTREAM_CHECK)
+#include "ngx_stream_upstream_check_module.h"
+#endif
 
 #define ngx_stream_upstream_tries(p) ((p)->number                             \
                                       + ((p)->next ? (p)->next->number : 0))
@@ -101,9 +104,20 @@ ngx_stream_upstream_init_round_robin(ngx_conf_t *cf,
                 peer[n].max_conns = server[i].max_conns;
                 peer[n].max_fails = server[i].max_fails;
                 peer[n].fail_timeout = server[i].fail_timeout;
+#if (NGX_STREAM_UPSTREAM_CHECK)
+		peer[n].check_index = (ngx_uint_t) NGX_ERROR;
+#endif
                 peer[n].down = server[i].down;
                 peer[n].server = server[i].name;
 
+#if (NGX_STREAM_UPSTREAM_CHECK)
+		if (!server[i].down) {
+			peer[n].check_index =
+				ngx_stream_upstream_check_add_peer(cf, us, &server[i].addrs[j]);
+		} else {
+			peer[n].check_index = (ngx_uint_t) NGX_ERROR;
+		}
+#endif
                 *peerp = &peer[n];
                 peerp = &peer[n].next;
                 n++;
@@ -451,6 +465,11 @@ ngx_stream_upstream_get_round_robin_peer(ngx_peer_connection_t *pc, void *data)
         if (peer->max_conns && peer->conns >= peer->max_conns) {
             goto failed;
         }
+#if (NGX_STREAM_UPSTREAM_CHECK)
+	if (ngx_stream_upstream_check_peer_down(peer->check_index)) {
+		goto failed;
+	}
+#endif
 
         rrp->current = peer;
 
@@ -546,6 +565,11 @@ ngx_stream_upstream_get_peer(ngx_stream_upstream_rr_peer_data_t *rrp)
             continue;
         }
 
+#if (NGX_STREAM_UPSTREAM_CHECK)
+	if (ngx_stream_upstream_check_peer_down(peer->check_index)) {
+		continue;
+	}
+#endif
         if (peer->max_fails
             && peer->fails >= peer->max_fails
             && now - peer->checked <= peer->fail_timeout)
